343870dd3f8fa5c4022652df8af40a4336225d06
ninotvaradze98-commits <ninotvaradze98@gmail.com>
2025-12-24 20:06:03 +0400

    updated DATA_URL (Now it works)

--- covid_src/config.py
+++ covid_src/config.py
@@ -11,5 +11,5 @@ class Configuration:
 
     BASE_FILE = Path(__file__).resolve().parent.parent #final_project
     DATA_PATH = BASE_FILE / "covid_data" / "covid-data.csv"
-    DATA_URL = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+    DATA_URL = "https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv"
 
